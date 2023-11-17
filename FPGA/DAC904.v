1b90b08ff45ad09b36615d1c1c182bc82fa6d6c8
Mike Cornelius <mike.cornelius@netcommwireless.com>
2023-11-17 14:55:10 +1100

    Add sine generator

--- FPGA/DAC904.v
+++ FPGA/DAC904.v
@@ -2,11 +2,24 @@ module DAC904 (input clk,
 					output dac904_clk,
 					output reg [13:0] dac904_data);
 
+	wire [13:0] data; 
+	//sine_wave_generator gen(clk, 60000, data); // 50.780 MHz
+	//sine_wave_generator gen(clk, 1000, data); // 2.929 MHz
+	//sine_wave_generator gen(clk, 2000, data); // 5,859 MHz
+	sine_wave_generator gen(clk, 3000, data); // 8.789 MHz
+	//sine_wave_generator gen(clk, 3300, data); // 27.831 MHz
+	//sine_wave_generator gen(clk, 4000, data); // 11.718 MHz
+	
 	always @(negedge clk) begin
-		dac904_data <= dac904_data +1;
+		//	dac904_data <= dac904_data +1;
 		//dac904_data <= 14'h3FFF; // 680mV
 		//dac904_data <= 14'h0; // -370mV
+		
+		
+		dac904_data <= data;
 	end
+	
+	
 	
 	assign dac904_clk = clk;
 					
---
+++ FPGA/sine_gen.v
@@ -0,0 +1,19 @@
+module sine_wave_generator (
+    input wire clk,                   // 125 MHz clock input
+    input wire [15:0] freq_control,   // 16-bit frequency control input
+    output wire [13:0] sine_out       // 14-bit sine wave output
+);
+
+reg [31:0] phase_accumulator = 0; // Adjust size as needed for precision
+
+always @(posedge clk) begin
+    phase_accumulator <= phase_accumulator + freq_control;
+end
+
+reg [13:0] sine_lut[0:1023]; 
+initial $readmemh ("sine_wave_lut_values.txt", sine_lut);
+
+//assign sine_out = sine_lut[phase_accumulator[31:22]]; 
+assign sine_out = sine_lut[phase_accumulator[9:0]]; 
+
+endmodule
